564c6da4d4618341350a2604eff2b361f0171123
Nikita <girmanik.nik@gmail.com>
2023-05-10 13:22:03 +0300

    bot update

--- bot/models/experiments.py
+++ bot/models/experiments.py
@@ -10,13 +10,13 @@ async def text_to_video(text: str, img: str):
     engine = pyttsx3.init()
     engine.setProperty('voice', 'ru')
     engine.setProperty("rate", 260)
-    engine.save_to_file(text, 'audio.mp3')
+    engine.save_to_file(text, 'temp/audio.mp3')
     engine.runAndWait()
-    audio_clip = mp.AudioFileClip('audio.mp3')
+    audio_clip = mp.AudioFileClip('temp/audio.mp3')
     video_clip = mp.ImageClip(img).set_audio(audio_clip)\
         .set_duration(audio_clip.duration)
-    video_clip.write_videofile("output.mp4", fps=24)
-    os.remove("audio.mp3")
+    video_clip.write_videofile("temp/output.mp4", fps=24)
+    os.remove("temp/audio.mp3")
 
 
 async def translate(text: str):
---
+++ training/launch_train.sh
@@ -0,0 +1,5 @@
+echo "Training model"
+
+python3 train_model.py -ml_config train_config.json
+
+echo "Training finished"
